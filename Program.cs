b50ec3f524bc3e0fc5b27863654982a485965b63
zhouwinsor <56124717+zhouwinsor@users.noreply.github.com>
2024-03-07 12:50:36 -0500

    class 3/7

--- Program.cs
+++ Program.cs
@@ -9,8 +9,22 @@ DiceGame is dependent on DiceCup
 I'm confused about the test methods :(
 */
 Console.WriteLine("Hello, World!");
-IDie die = new StandardDie(4);
-die.Roll();
+
+IDie die = StandardDie.D4;
+IDie die2 = StandardDie.D4;
+IDie die3 = StandardDie.D4;
+
+var dices = new DiceCup(StandardDie.D4, StandardDie.D4, StandardDie.D4);
+
+Console.WriteLine(dices.RollAll());
+
+/*int[] rollCounts = [0, 0, 0, 0];
+for (var i = 0; i < 10_000; i++)
+{
+    rollCounts[die.Roll() - 1]++;
+}
+Console.WriteLine($"{rollCounts[0]},{rollCounts[1]}, {rollCounts[2]}, {rollCounts[3]} ");
+*/
 
 
 
